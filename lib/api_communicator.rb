bca3b53358e869bb66f1428afe44a709417c481e
Eric Razor, Jr <eric.razorjr@gmail.com>
2018-05-16 00:17:53 -0400

    almost finished parse_character_movies. Just need to figure out formatting

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,13 +6,16 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-
+  # character_hash['results'].each do |the_character, character_data|
+  #   puts "#{the_character}"
+  #   binding.pry
+  #
+  # end
   character_data = character_hash['results']
   selected_character = character_data.find do |character_info|
       character_info['name'] == character
   end
   return selected_character["films"]
-
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -24,19 +27,26 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-def parse_character_movies(films_array) #films_hash
+def parse_character_movies(character_name)
+  counter = 0
   # some iteration magic and puts out the movies in a nice list
-  # films_array.collect do |film|
-  #   scraped_films = RestClient.get(film)
-  #   parsed_films = JSON.parse(scraped_films)
-  # end
-  #JSON.parse(RestClient.get(films_array[0]))['title']
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  character_hash = JSON.parse(all_characters)
+
+  char_data = character_hash['results']
+  selected_char = char_data.find do |character|
+    character['name'] == character_name
+    while counter < character['films'].length do
+      puts "#{counter}. #{character['films']}"
+      counter += 1
+    end
+  end
   binding.pry
 end
 
 def show_character_movies(character)
-  films_array = get_character_movies_from_api(character)
-  parse_character_movies(films_array)
+  films_hash = get_character_movies_from_api(character)
+  parse_character_movies(films_hash)
 end
 
 ## BONUS
@@ -44,4 +54,5 @@ end
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
 
-show_character_movies('Luke Skywalker')
+#puts get_character_movies_from_api('Luke Skywalker')
+puts parse_character_movies('Luke Skywalker')
