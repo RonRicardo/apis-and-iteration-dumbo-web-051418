b9d40e7426d48d8fdf46dcecefd7271bd586afb2
github username <github email address>
2018-05-16 12:16:11 -0400

    done -- started bonus

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,13 +3,17 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
+  #parsing all characters
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
 
+  #finding the character we passed in
   character_data = character_hash['results']
   selected_character = character_data.find do |character_info|
-      character_info['name'] == character
+      character_info['name'].downcase == character
   end
+
+  #collecting the list of films for the character
   char_films = selected_character["films"]
   films_hash = char_films.collect do |film|
     RestClient.get(film)
@@ -36,3 +40,32 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+# def parsing_all_characters(api='http://www.swapi.co/api/people/')
+#   all_characters = RestClient.get(api)
+#   character_hash = JSON.parse(all_characters)
+# end
+#
+# def find_char(character_hash)
+#   character_data = character_hash['results']
+#   selected_character = character_data.find do |character_info|
+#       character_info['name'].downcase == character
+#   end
+#   selected_character
+# end
+#
+# def list_films(selected_char)
+#   char_films = selected_char["films"]
+#   films_hash = char_films.collect do |film|
+#     RestClient.get(film)
+#   end
+#   films_hash
+# end
+#
+# def bonus_show_character_movies(character)
+#   all_characters = parsing_all_characters
+#   find_char(all_characters)
+#   films_hash = list_films(find_char)
+#   parse_character_movies(films_hash)
+#   binding.pry
+# end
