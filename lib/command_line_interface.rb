8f6d7ac2c203aff63d6681efebb452247094ed32
github username <github email address>
2018-05-16 11:20:05 -0400

    fixed issue where welcome message was printing twice

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,8 @@
 def welcome
-  get_character_from_user
+  puts "please enter a character"
 end
 
 def get_character_from_user
-  puts "please enter a character"
   user_input = gets.chomp
   user_input.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
